1a774e4b72ebe932ddb117899f387f9a0f2d973b
KononenkoElisaveta <bebebelisa1@gmail.com>
2026-03-24 03:02:41 +0300

    add file

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -5,7 +5,7 @@ def zd1():
     else:
         print("net")
 
-def zd3():
+def zd2():
     try:
         s=int(input("введитее число: "))
         print(100/s)
@@ -16,12 +16,35 @@ def zd3():
 
 def zd3():
     s=input("введите дату (дд.мм.гггг)")
-    d,m,y=s.split('.')
+    d,m,y= s.split ('.')
     d=int(d)
     m=int(m)
     y=int(y)
-    if d*m==y%100:
+    if d * m == y % 100:
         print(True)
     else:
         print(False)
 def zd4():
+    sum1=sum2=0
+    s = input("введите число: ")
+    if len(s) % 2 == 0:
+        for i in range(len(s)):
+            if i < len(s) // 2:
+                sum1+=int(s[i])
+            else:
+                sum2+=int(s[i])
+        if sum1 == sum2:
+            print (True)
+        else:
+            print(False)
+    else:
+        print("оличество цифр должно быть четным")
+
+print("задание 1")
+zd1()
+print("задание 2")
+zd2()
+print("задание 3")
+zd3()
+print("задание 4")
+zd4()
